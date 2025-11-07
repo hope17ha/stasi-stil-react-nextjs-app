9ccc5cc13999145b5e36eb78eba9bf6694a0231e
hope17ha <n.anastasova91@gmail.com>
2025-11-07 12:00:36 +0200

    fixed: layout jsx for browser rendering components for dynamic page title

---
+++ stasi-stil/app/gallery/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Галерия | СТАСИ СТИЛ ФРИЗЬОРСКИ САЛОН'
+  };
+
+  
+export default function GalleryLayout({ children }){
+    return (
+        <>
+          {children}
+        </>
+      );
+}
---
+++ stasi-stil/app/reviews/layout.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: 'Отзиви | СТАСИ СТИЛ ФРИЗЬОРСКИ САЛОН'
+  };
+
+export default function ReviewsLayout ({ children }){
+    return (
+        <>
+        {children}
+        </>
+    )
+};
